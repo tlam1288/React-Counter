23d8c7bf6fe364b35c456c8ad08eea3e57cbd907
nhuanthi <tlam1288@gmail.com>
2020-10-21 14:22:58 -0700

    created calculateTimeLeft() to find current time/day

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
 
-import './App.css';
+
 
 function App() {
+
+const calculateTimeLeft = ()=>{
+  let year = new Date().getFullYear();
+
+  const difference = +new Date(`12/16/${year}`) - +new Date();
+
+  let timeLeft = {};
+  if(difference > 0){
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 *24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60)
+    };
+  }
+
+  return timeLeft;
+}
+
   return (
-    <div>
-      <header>
-        <h1>CRA Deploy</h1>
-      </header>
-      <p>
-        lorem15 
-      </p>
-    </div>
+    <></>
   );
 }
 
